12a4d6417b7e23aaff05d7afe6890f648cef4a56
Juhwan Choi <jumax108@kakao.com>
2021-06-01 16:49:04 +0900

    add files

---
+++ simple_text_parser/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+#include "myLinkedList.h"
+#include "TextParser.h"
+
+int main() {
+
+	TextParser textParser("test.txt");
+
+	char* text = nullptr;
+
+	textParser.GetValueByKey("bbb", &text);
+	textParser.GetTextByKey("ccc", &text);
+	printf("%s", text);
+
+	return 0;
+}
